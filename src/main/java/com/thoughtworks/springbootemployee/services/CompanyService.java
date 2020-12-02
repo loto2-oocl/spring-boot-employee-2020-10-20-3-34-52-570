4b7ddd78dd7dbb188f72eedf43e3b97a61f9d64d
Tom Lo <loto2@oocl.com>
2020-12-02 22:44:45 +0800

    [company][Tom] feat: return all employees under company when getCompanyEmployees given company id

--- src/main/java/com/thoughtworks/springbootemployee/services/CompanyService.java
+++ src/main/java/com/thoughtworks/springbootemployee/services/CompanyService.java
@@ -22,6 +22,6 @@ public class CompanyService {
     }
 
     public List<Employee> getCompanyEmployees(Integer companyId) {
-        return null;
+        return this.companyRepository.findById(companyId).getEmployees();
     }
 }
