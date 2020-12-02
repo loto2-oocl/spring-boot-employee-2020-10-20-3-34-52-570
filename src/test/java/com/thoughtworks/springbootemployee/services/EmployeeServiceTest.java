4290163cbcd2b4f7aa9d8ad2de5e96b2660f94d2
Tom Lo <loto2@oocl.com>
2020-12-02 20:17:32 +0800

    [employee][Tom] refactor: use MockBean and InjectMocks in tests

--- src/test/java/com/thoughtworks/springbootemployee/services/EmployeeServiceTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/services/EmployeeServiceTest.java
@@ -3,21 +3,31 @@ package com.thoughtworks.springbootemployee.services;
 import com.thoughtworks.springbootemployee.entity.Employee;
 import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class EmployeeServiceTest {
+    @InjectMocks
+    EmployeeService employeeService;
+
+    @MockBean
+    EmployeeRepository employeeRepository;
+
     @Test
     public void should_return_all_employee_when_get_all_given_repository_with_all_employee() {
         //given
-        EmployeeRepository employeeRepository = Mockito.mock(EmployeeRepository.class);
-        EmployeeService employeeService = new EmployeeService(employeeRepository);
         List<Employee> expected = Arrays.asList(new Employee(), new Employee());
         when(employeeRepository.findAll()).thenReturn(expected);
 
@@ -31,9 +41,7 @@ class EmployeeServiceTest {
     @Test
     void should_return_created_employee_when_create_given_an_empty_repository_and_employee_request() {
         //given
-        EmployeeRepository employeeRepository = Mockito.mock(EmployeeRepository.class);
-        EmployeeService employeeService = new EmployeeService(employeeRepository);
-        Employee expected = new Employee();
+        Employee expected = new Employee(1, "Tom", 18, "male", 10000);
         when(employeeRepository.create(any())).thenReturn(expected);
 
         //when
