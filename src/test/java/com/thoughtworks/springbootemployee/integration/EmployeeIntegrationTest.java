d8f2deccd35c5898d56aa3afb5f3d20a13aceb89
Tom Lo <loto2@oocl.com>
2020-12-03 17:52:44 +0800

    [spring-boot-employee][tom] test: integration test to obtain employee list

--- src/test/java/com/thoughtworks/springbootemployee/integration/EmployeeIntegrationTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/integration/EmployeeIntegrationTest.java
@@ -1,11 +1,17 @@
 package com.thoughtworks.springbootemployee.integration;
 
+import com.thoughtworks.springbootemployee.entity.Employee;
 import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -19,5 +25,22 @@ public class EmployeeIntegrationTest {
     @AfterEach
     void tearDown() {
         employeeRepository.deleteAll();
+    }
+
+    @Test
+    void should_return_employees_when_called_get_all_give_employees() throws Exception {
+        //given
+        Employee employee = new Employee("Tom", 18, "Male", 10000);
+        employeeRepository.insert(employee);
+
+        //when
+        mockMvc.perform(get("/employees"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$[0].id").isString())
+                .andExpect(jsonPath("$[0].name").value("Tom"))
+                .andExpect(jsonPath("$[0].age").value(18))
+                .andExpect(jsonPath("$[0].gender").value("Male"))
+                .andExpect(jsonPath("$[0].salary").value(10000));
     }
 }
