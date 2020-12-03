4040bf84a65789b247fcbe12bf7853f67762c7ad
Tom Lo <loto2@oocl.com>
2020-12-03 18:40:04 +0800

    [spring-boot-employee][tom] test: integration test to obtain employee list by page and pageSize

--- src/test/java/com/thoughtworks/springbootemployee/integration/EmployeeIntegrationTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/integration/EmployeeIntegrationTest.java
@@ -9,6 +9,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Arrays;
+
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -58,5 +61,31 @@ public class EmployeeIntegrationTest {
                 .andExpect(jsonPath("$.age").value(18))
                 .andExpect(jsonPath("$.gender").value("Male"))
                 .andExpect(jsonPath("$.salary").value(10000));
+    }
+
+    @Test
+    void should_return_2_employee_when_called_get_all_paginated_given_3_employees_page_1_page_size_2() throws Exception {
+        //given
+        Employee employee1 = new Employee("Tom", 18, "Male", 10000);
+        Employee employee2 = new Employee("Tom1", 19, "Male", 10001);
+        Employee employee3 = new Employee("Tom2", 20, "Male", 10002);
+        employeeRepository.insert(Arrays.asList(employee1, employee2, employee3));
+
+        //when
+        mockMvc.perform(get("/employees")
+                    .param("page", "1")
+                    .param("pageSize", "2"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].id").isString())
+                .andExpect(jsonPath("$[0].name").value("Tom"))
+                .andExpect(jsonPath("$[0].age").value(18))
+                .andExpect(jsonPath("$[0].gender").value("Male"))
+                .andExpect(jsonPath("$[0].salary").value(10000))
+                .andExpect(jsonPath("$[1].id").isString())
+                .andExpect(jsonPath("$[1].name").value("Tom1"))
+                .andExpect(jsonPath("$[1].age").value(19))
+                .andExpect(jsonPath("$[1].gender").value("Male"))
+                .andExpect(jsonPath("$[1].salary").value(10001));
     }
 }
