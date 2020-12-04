bd91c951a721e8428a1da158b31c2c2c0ff490ac
Tom Lo <loto2@oocl.com>
2020-12-04 15:29:17 +0800

    [spring-boot-employee][tom] feat: employee use dto mapper pattern

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -1,51 +1,81 @@
 package com.thoughtworks.springbootemployee.controller;
 
+import com.thoughtworks.springbootemployee.dto.EmployeeRequest;
+import com.thoughtworks.springbootemployee.dto.EmployeeResponse;
 import com.thoughtworks.springbootemployee.entity.Employee;
+import com.thoughtworks.springbootemployee.mapper.EmployeeMapper;
 import com.thoughtworks.springbootemployee.services.EmployeeService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
-    @Autowired
-    private EmployeeService employeeService;
+    private final EmployeeService employeeService;
+
+    private final EmployeeMapper employeeMapper;
+
+    public EmployeeController(EmployeeService employeeService, EmployeeMapper employeeMapper) {
+        this.employeeService = employeeService;
+        this.employeeMapper = employeeMapper;
+    }
 
     @GetMapping
-    public List<Employee> getAll() {
-        return this.employeeService.getAll();
+    public List<EmployeeResponse> getAll() {
+        return this.employeeService.getAll().stream()
+            .map(employeeMapper::toResponse)
+            .collect(Collectors.toList());
     }
 
     @GetMapping(params = {
-            "page",
-            "pageSize"
+        "page",
+        "pageSize"
     })
-    public List<Employee> getAllPaginated(@RequestParam("page") Integer page, @RequestParam("pageSize") Integer pageSize) {
-        return this.employeeService.getAllPaginated(page, pageSize).getContent();
+    public List<EmployeeResponse> getAllPaginated(@RequestParam("page") Integer page, @RequestParam("pageSize") Integer pageSize) {
+        return this.employeeService.getAllPaginated(page, pageSize).getContent().stream()
+            .map(employeeMapper::toResponse)
+            .collect(Collectors.toList());
     }
 
     @GetMapping(params = "gender")
-    public List<Employee> getAllByGender(@RequestParam("gender") String gender) {
-        return this.employeeService.getAllByGender(gender);
+    public List<EmployeeResponse> getAllByGender(@RequestParam("gender") String gender) {
+        return this.employeeService.getAllByGender(gender).stream()
+            .map(employeeMapper::toResponse)
+            .collect(Collectors.toList());
     }
 
     @GetMapping("/{employeeId}")
-    public Employee findEmployee(@PathVariable String  employeeId) {
-        return this.employeeService.getOneById(employeeId);
+    public EmployeeResponse findEmployee(@PathVariable String employeeId) {
+        Employee employee = this.employeeService.getOneById(employeeId);
+
+        return employeeMapper.toResponse(employee);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Employee create(@RequestBody Employee employee) {
-        return this.employeeService.create(employee);
+    public EmployeeResponse create(@RequestBody EmployeeRequest employeeRequest) {
+        Employee employee = this.employeeService.create(employeeMapper.toEntity(employeeRequest));
+
+        return employeeMapper.toResponse(employee);
     }
 
     @PutMapping("/{employeeId}")
-    public Employee update(@PathVariable String employeeId, @RequestBody Employee employeeUpdate) {
-        return this.employeeService.update(employeeId, employeeUpdate);
+    public EmployeeResponse update(@PathVariable String employeeId, @RequestBody EmployeeRequest employeeRequest) {
+        Employee employee = this.employeeService.update(employeeId, employeeMapper.toEntity(employeeRequest));
+
+        return employeeMapper.toResponse(employee);
     }
 
     @DeleteMapping("/{employeeId}")
---
+++ src/main/java/com/thoughtworks/springbootemployee/dto/EmployeeResponse.java
@@ -0,0 +1,70 @@
+package com.thoughtworks.springbootemployee.dto;
+
+public class EmployeeResponse {
+    private String id;
+    private String name;
+    private Integer age;
+    private String gender;
+    private Integer salary;
+    private String companyId;
+
+    public EmployeeResponse() {
+    }
+
+    public EmployeeResponse(String id, String name, Integer age, String gender, Integer salary, String companyId) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.salary = salary;
+        this.companyId = companyId;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+
+    public String getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(String companyId) {
+        this.companyId = companyId;
+    }
+}
