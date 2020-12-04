0614bf1becbaab84c4b946aa4753920e8300cb06
Tom Lo <loto2@oocl.com>
2020-12-04 14:06:48 +0800

    [spring-boot-employee][tom] refactor: separate advice by employee and company

--- src/main/java/com/thoughtworks/springbootemployee/advice/CompanyControllerAdvice.java
+++ src/main/java/com/thoughtworks/springbootemployee/advice/CompanyControllerAdvice.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.springbootemployee.advice;
 
+import com.thoughtworks.springbootemployee.exceptions.CompanyNotFoundException;
 import com.thoughtworks.springbootemployee.response.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -7,10 +8,10 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class GlobalControllerAdvice {
+public class CompanyControllerAdvice {
     @ExceptionHandler
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handIllegalArgumentException(IllegalArgumentException exception){
-        return new ErrorResponse(exception.getMessage(), HttpStatus.BAD_REQUEST.name());
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleCompanyNotFoundException(CompanyNotFoundException companyNotFoundException) {
+        return new ErrorResponse(companyNotFoundException.getMessage(), HttpStatus.NOT_FOUND.name());
     }
 }
--- src/main/java/com/thoughtworks/springbootemployee/advice/EmployeeControllerAdvice.java
+++ src/main/java/com/thoughtworks/springbootemployee/advice/EmployeeControllerAdvice.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.springbootemployee.advice;
 
+import com.thoughtworks.springbootemployee.exceptions.EmployeeNotFoundException;
 import com.thoughtworks.springbootemployee.response.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -7,10 +8,10 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class GlobalControllerAdvice {
+public class EmployeeControllerAdvice {
     @ExceptionHandler
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handIllegalArgumentException(IllegalArgumentException exception){
-        return new ErrorResponse(exception.getMessage(), HttpStatus.BAD_REQUEST.name());
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleEmployeeNotFoundException(EmployeeNotFoundException employeeNotFoundException) {
+        return new ErrorResponse(employeeNotFoundException.getMessage(), HttpStatus.NOT_FOUND.name());
     }
 }
